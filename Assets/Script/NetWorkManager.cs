8516bf5014390e8b8d7e6d985223d570e5ac6185
DergeRice <114036224+DergeRice@users.noreply.github.com>
2023-08-12 13:35:46 +0900

    auto refresh enabled

--- Assets/Script/NetWorkManager.cs
+++ Assets/Script/NetWorkManager.cs
@@ -11,13 +11,43 @@ public class NetWorkManager : MonoBehaviourPunCallbacks
         PhotonNetwork.SendRate = 60;
         PhotonNetwork.SerializationRate = 30;
 
+        //Connect();
+        //nConnectedToMaster();
+
         
     }
 
-    public void Connect() => PhotonNetwork.ConnectUsingSettings();
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    //public void Connect() => PhotonNetwork.ConnectUsingSettings();
 
     public override void OnJoinedRoom()
     {
-       
+        Debug.Log("JoinedRoom");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnConnected()
+    {
+        
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("EnterLobby");
+        PhotonNetwork.JoinOrCreateRoom("Default", new RoomOptions { MaxPlayers = 2 }, null);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("CreatedRoom");
     }
 }
